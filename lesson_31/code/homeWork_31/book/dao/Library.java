3ea00e246820b28f6dea9c78a208c6b506920d6d
Rydzaki <sibens@mail.ru>
2023-10-19 12:49:44 +0200

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	lesson_31/code/homeWork_31/book/dao/Library.java
    #	lesson_31/code/homeWork_31/book/dao/LibraryImpl.java
    #	lesson_31/code/homeWork_31/book/tests/LibraryImplTest.java

--- lesson_31/code/homeWork_31/book/dao/Library.java
+++ lesson_31/code/homeWork_31/book/dao/Library.java
@@ -4,11 +4,13 @@ import homeWork_31.book.model.Book;
 
 public interface Library {
 
-    boolean addBook (Book book);
-    Book removeBook (int isbn);
-    Book findBook (int isbn);
-    int quantity();
-    void printAllBooks ();
+    boolean addBook(Book book);
 
-//    void printAllBooks();
+    Book removeBook(int isbn);
+
+    Book findBook(int isbn);
+
+    int quantity();
+
+    void printAllBooks();
 }
