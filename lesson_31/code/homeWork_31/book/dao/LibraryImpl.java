1e6e4ab0e6e9d36892e312a20f3a126e4d86b7ba
Rydzaki <sibens@mail.ru>
2023-10-19 23:36:40 +0200

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	lesson_31/code/homeWork_31/book/dao/Library.java
    #	lesson_31/code/homeWork_31/book/dao/LibraryImpl.java
    #	lesson_31/code/homeWork_31/book/tests/LibraryImplTest.java

--- lesson_31/code/homeWork_31/book/dao/LibraryImpl.java
+++ lesson_31/code/homeWork_31/book/dao/LibraryImpl.java
@@ -2,23 +2,25 @@ package homeWork_31.book.dao;
 
 import homeWork_31.book.model.Book;
 
+import java.util.Arrays;
+
 public class LibraryImpl implements Library {
 
     private Book[] books;
     private int totalSize; // всего книг в библиотеке
 
-    public LibraryImpl (int capacity){
+    public LibraryImpl(int capacity) {
         books = new Book[capacity]; // макс книг в библиотеке
     }
 
 
     @Override
     public boolean addBook(Book book) {
-        if(book == null || totalSize == books.length || findBook(book.getIsbn()) != null){
+        if (book == null || totalSize == books.length || findBook(book.getIsbn()) != null) {
             return false;
         }
         books[totalSize] = book;
-        totalSize ++;
+        totalSize++;
 
         return true;
     }
@@ -26,11 +28,11 @@ public class LibraryImpl implements Library {
     @Override
     public Book removeBook(int isbn) {
         for (int i = 0; i < totalSize; i++) {
-            if(books[i].getIsbn() == isbn){
+            if (books[i].getIsbn() == isbn) {
                 Book vic = books[i]; // убрали найденный элемент в переменную
                 books[i] = books[totalSize - 1]; //поставили на место найденого элемента последний
                 books[totalSize - 1] = null; // затерли дубля последнего элемента
-                totalSize --;
+                totalSize--;
                 return vic;
             }
         }
@@ -41,7 +43,7 @@ public class LibraryImpl implements Library {
     @Override
     public Book findBook(int isbn) {
         for (int i = 0; i < totalSize; i++) {
-            if (books[i].getIsbn() == isbn){
+            if (books[i].getIsbn() == isbn) {
                 return books[i];
             }
         }
@@ -62,4 +64,24 @@ public class LibraryImpl implements Library {
 
     }
 
-}
+    @Override
+    public Book[] findAuthor(String author) {
+        int count = 0;
+        for (int i = 0; i < totalSize; i++) {
+            if (books[i].getAuthor().equals(author)) {
+                count++;
+            }
+        }
+        Book[] res = new Book[count];
+        for  (int i = 0, j = 0; j < res.length; i++) {
+            if (books[i].getAuthor().equals(author)) {
+                res[j++] = books[i];
+                }
+            }
+        Arrays.asList(res).forEach(System.out::println); // !!! печатает каждый элемент массива с новой строки
+//        System.out.println(Arrays.toString(res));
+            return res;
+        }
+
+    }
+
