e0323f082a761059c73250a94c841f4aa3096d19
Rydzaki <sibens@mail.ru>
2023-10-24 21:56:14 +0200

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	lesson_31/code/homeWork_31/book/dao/Library.java
    #	lesson_31/code/homeWork_31/book/dao/LibraryImpl.java
    #	lesson_31/code/homeWork_31/book/tests/LibraryImplTest.java

---
+++ lesson_35/code/practice/soldier/model/Soidier.java
@@ -0,0 +1,64 @@
+package practice.soldier.model;
+
+public class Soidier implements Comparable<Soidier> {
+
+    private String name;
+    int height;
+    double weight;
+    private int profile;
+
+    public Soidier(String name, int height, double weight, int profile) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.profile = profile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getProfile() {
+        return profile;
+    }
+
+    public void setProfile(int profile) {
+        this.profile = profile;
+    }
+
+    @Override
+    public String toString() {
+        return "Soidier{" +
+                "name='" + name + '\'' +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", profile=" + profile +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Soidier o) {
+        return this.height - o.height;
+    }
+
+}
