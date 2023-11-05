8933118ba5fffee0c9c2729665c0562c002374f6
Anton Ushakov <sibens@mail.ru>
2023-11-05 15:47:41 +0100

    Home Work 41

---
+++ lesson_41_album/code/practice03_11/album/dao/Album.java
@@ -0,0 +1,18 @@
+package practice03_11.album.dao;
+
+import practice03_11.album.model.Photo;
+
+import java.time.LocalDate;
+
+public interface Album {
+
+    boolean addPhoto (Photo photo);
+    boolean removePhoto(int photoId, int albumId);
+    boolean updatePhoto(int photoId, int albumId, String url);
+    Photo getPhotoFromAlbum(int photoId, int albumId);
+    Photo[] getAllPhotoFromAlbum(int albumId);
+    Photo[] getPhotoBetweenDate(LocalDate dateFrom, LocalDate dateTo);
+    int size(); // quantity of photos in all albums
+
+
+}
