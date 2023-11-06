95525d4669bfaf890bb59f92733a3266609f71a6
Anton Ushakov <sibens@mail.ru>
2023-11-06 10:34:47 +0100

    master update

--- lesson_43_2_GIT/code/prictase_test/Test2.java
+++ lesson_43_2_GIT/code/prictase_test/Test2.java
@@ -5,5 +5,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("test4");
         System.out.println("привет мир!");
+        System.out.println("master update");
     }
 }
